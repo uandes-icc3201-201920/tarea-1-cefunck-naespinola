5e80555e104deafc4bf3f906b044fb6d27cf56a4
Cristian Funck <cefunck@miuandes.cl>
2019-09-02 16:58:28 -0400

    server actualizado: implementación de función insert por ahora sin validación de claves

--- server.cpp
+++ server.cpp
@@ -10,11 +10,19 @@ using namespace std;
 // Almacenamiento KV
 KVStore db;
 
-int main(int argc, char** argv) {	
-	
+// Función insert por ahora sin validación de las claves
+int insert(unsigned long key, string data){
+	vector<byte> vdata(data.begin(),data.end());
+	Value value = { data.size(), vdata};
+	db.insert(std::pair<unsigned long, Value>(key, value));
+	return 0;
+}
+
+int main(int argc, char** argv) {
+
 	int sflag = 0;
 	int opt;
-	
+
 	// Procesar opciones de linea de comando
     while ((opt = getopt (argc, argv, "s:")) != -1) {
         switch (opt)
@@ -25,29 +33,36 @@ int main(int argc, char** argv) {
 				break;
 			default:
 				return EXIT_FAILURE;
-          }	    	
+          }
     }
-	
-	// Uso elemental del almacenamiento KV:
-	
-	// Creamos un arreglo de bytes a mano
-	byte data[] = { 0x01, 0x01, 0x01, 0x01, 0x01 };
 
+	// Uso elemental del almacenamiento KV:
+
+	// Creamos un arreglo de bytes a mano
+	//byte data[] = { 0x0F, 0x01, 0x01, 0x01, 0x01 };
+	string data = "abcde";
 	// Luego un vector utilizando el arreglo de bytes
-	vector<byte> vdata(data, data + sizeof(data));
-	
+	//vector<byte> vdata(data, data + sizeof(data));
+	//vector<byte> vdatastr(datastr.begin(),datastr.end());
+
 	// Creamos el valor
-	Value val = { 5, vdata };
-	
+	//Value val = { sizeof(data), vdata };
+	//Value valstr = { datastr.size(), vdatastr};
+
 	// Insertamos un par clave, valor directamente
 	// en el mapa KV
-	
+
 	// Nota: Debiera diseñarse una solución más robusta con una interfaz
 	// adecuada para acceder a la estructura.
-	db.insert(std::pair<unsigned long, Value>(1000, val));
-		
+	//db.insert(std::pair<unsigned long, Value>(1000, valstr));
+
+	insert(1000,data);
+
 	// Imprimir lo que hemos agregado al mapa KV.
 	cout << db[1000].size << " " << (int) db[1000].data[0] << endl;
-	
+	// esto de abajo es un ejemplo de recuperación del string que está en el arreglo de bytes
+	string str_saved(db[1000].data.begin(),db[1000].data.end());
+	cout<< (str_saved.data()) << " size:"<< str_saved.size() << endl;
+
 	return 0;
 }
