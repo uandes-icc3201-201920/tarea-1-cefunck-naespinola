0d6409ae590dfbc5737251150780660a1b4f51db
Cristian Funck <cefunck@miuandes.cl>
2019-09-03 16:27:59 -0400

    server.cpp actualizado: Implementación de las operaciones insert en servidor con contador global y validación de claves

--- server.cpp
+++ server.cpp
@@ -3,20 +3,58 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <ctime>
 #include "util.h"
 
 using namespace std;
 
 // Almacenamiento KV
 KVStore db;
+unsigned long global_key_counter;
 
-// Función insert por ahora sin validación de las claves
+// Función que retorna 1 si la clave ya existe y 0 si no.
+int is_an_existing_key(unsigned long new_key, KVStore db){
+	for(map<unsigned long, Value>::iterator it = db.begin(); it != db.end(); ++it) {
+		if(it->first == new_key){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+// Función que retorna una clave aleatorea entera entre 10.000 y 1000.
+unsigned long random_key(){
+	srand((unsigned)time(0));
+	unsigned long i;
+	i = (rand()%10000)+1000;
+	return i;
+}
+
+// Función que inicializa la base de datos.
+void init_db(){
+	global_key_counter = random_key();
+}
+
+// Función que inserta un dato en la clave indicada, validando primero si ya
+// existiese la clave, en tal caso no la inserta y retorna -1, en caso contrario
+// la inserta exitosamente el dato en la clave indicada y retorna la clave.
 int insert_into_db(unsigned long key, string data){
+	if (is_an_existing_key(key, db)){
+		return -1;
+	}
 	vector<byte> vdata(data.begin(),data.end());
 	Value value = { data.size(), vdata};
 	db.insert(pair<unsigned long, Value>(key, value));
-	return 0;
+	return key;
 }
+
+// Función que inserta un dato en la base de datos con clave autogenerada, si
+// la operación tiene exito retorna la clave generada.
+int insert_into_db(string data){
+	unsigned long new_key = global_key_counter++;
+	return insert_into_db(new_key, data);
+}
+
 
 int main(int argc, char** argv) {
 
@@ -36,28 +74,11 @@ int main(int argc, char** argv) {
           }
     }
 
-	// Uso elemental del almacenamiento KV:
-
-	// Creamos un arreglo de bytes a mano
-	//byte data[] = { 0x0F, 0x01, 0x01, 0x01, 0x01 };
+	init_db();
 	string data = "abcde";
-	// Luego un vector utilizando el arreglo de bytes
-	//vector<byte> vdata(data, data + sizeof(data));
-	//vector<byte> vdatastr(datastr.begin(),datastr.end());
-
-	// Creamos el valor
-	//Value val = { sizeof(data), vdata };
-	//Value valstr = { datastr.size(), vdatastr};
-
-	// Insertamos un par clave, valor directamente
-	// en el mapa KV
-
-	// Nota: Debiera diseñarse una solución más robusta con una interfaz
-	// adecuada para acceder a la estructura.
-	//db.insert(std::pair<unsigned long, Value>(1000, valstr));
-
-	insert_into_db(1000,data);
-
+	cout << insert_into_db(1000,data) << endl;
+	cout << insert_into_db(1001,data) << endl;
+	cout << insert_into_db(1001,data) << endl;
 	// Imprimir lo que hemos agregado al mapa KV.
 	cout << db[1000].size << " " << (int) db[1000].data[0] << endl;
 	// esto de abajo es un ejemplo de recuperación del string que está en el arreglo de bytes
