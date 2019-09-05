5650f829e85fabd1e1dc690b0f5510c6c655d3bf
NataliaEspinola <naespinola@miuandes.cl>
2019-09-04 22:40:21 -0400

    Se creo un socket en server.cpp y su vinculacion con client y server, falta implementar read y write correctamente

--- server.cpp
+++ server.cpp
@@ -7,6 +7,13 @@
 #include <math.h>
 #include "util.h"
 
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+
 using namespace std;
 
 // Almacenamiento KV
@@ -112,7 +119,13 @@ int insert_into_db(Value value){
 
 int main(int argc, char** argv) {
 
-	int sflag = 0;
+	struct sockaddr_un addr;
+  	char buf[100];
+  	int fd,cl,rc; //cl es client cocket, fd es server socket
+  	//int bind, listen, server_socket, accept, read; //posibles variables para sacar lo que esta dentro de los if
+
+	char *socket_path = "/tmp/db.tuples.sock"; //saque el punto que estaba al final
+
 	int opt;
 
 	// Procesar opciones de linea de comando
@@ -121,14 +134,61 @@ int main(int argc, char** argv) {
 		{
 			/* Procesar el flag s si el usuario lo ingresa */
 			case 's':
-				sflag = 1;
+				socket_path = optarg;	
 				break;
 			default:
 				return EXIT_FAILURE;
           }
     }
 
+    //Se crea un socket de tipo SCK_STREAM y protocolo 0
+    if ( (fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+	    perror("socket error");
+	    exit(-1);
+  	}
+
+  	memset(&addr, 0, sizeof(addr));
+  	addr.sun_family = AF_UNIX;
+	strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+    unlink(socket_path);
+
+    //Establecer vinculacion con la dirección local del socket
+	if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+	    perror("bind error");
+	    exit(-1);
+	}
+
+	//Escuchar conexion de cliente
+	if (listen(fd, 5) == -1) { //permite una cola de 5 conexiones
+	    perror("listen error");
+	    exit(-1);
+	}
+	cout << "socket listening..." << endl;
+
+	if ( (cl = accept(fd, NULL, NULL)) == -1) {
+	perror("accept error");
+	}
+
+
+	//Intento de read
+	/*
+	while (1) {
+	    while ( (rc=read(cl,buf,sizeof(buf))) > 0) {
+	    	printf("read %u bytes: %.*s\n", rc, rc, buf);
+	    }
+	    if (rc == -1) {
+	    	perror("read");
+			exit(-1);
+	    }
+	    else if (rc == 0) {
+	    	printf("EOF\n");
+	    	close(cl);
+	    }
+	}
+	*/
 	init_db();
+
+	cout << socket_path << endl;
 
 	Value value1 = string_to_value("primer dato");
 	Value value2 = string_to_value("segundo dato");
