bf3b5a1fe4c48ed34250be1bf77dce6b708eee80
NataliaEspinola <naespinola@miuandes.cl>
2019-09-17 19:41:28 -0300

    implementacion de la comunicacion entre server y client

--- client.cpp
+++ client.cpp
@@ -8,6 +8,26 @@
 
 using namespace std;
 
+int client_socket_file_descriptor(char *socket_path){
+	int fd;
+	struct sockaddr_un addr;
+
+	if ( (fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+		perror("socket error");
+		exit(-1);
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sun_family = AF_UNIX;
+	strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+
+	if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+	  	perror("connect error");
+	  	exit(-1);
+	}
+	return fd;
+}
+
 int main(int argc, char** argv) {
 	char *socket_path = "/tmp/db.tuples.sock"; //saque el punto que estaba al final
 	struct sockaddr_un addr;
@@ -27,36 +47,45 @@ int main(int argc, char** argv) {
           }
     }
 
-	if ( (fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-		perror("socket error");
-		exit(-1);
-	}
-	memset(&addr, 0, sizeof(addr));
-	addr.sun_family = AF_UNIX;
-	strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+  	fd = client_socket_file_descriptor(socket_path);
 	
-	if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-	  	perror("connect error");
-	  	exit(-1);
-	}
+	bool lectura = true;
+	while(1){
 
-	//intento de Read
-	while( (rc=read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
-    	if (write(fd, buf, rc) != rc) {
-    		if (rc > 0) fprintf(stderr,"partial write");
-    			else {
-    				perror("write error");
-        			exit(-1);
-      			}
-    	}
-  	}
-	
-	
-	string cmd = "";
-	
-	while (cmd != "quit") {
-		cout << ">";
-		cin >> cmd;
+		while(!lectura){
+			cout << "Entre al modo escritura en client" << endl;
+			while( (rc=read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
+				if (write(fd, buf, rc) != rc) {
+					if (rc > 0) fprintf(stderr,"partial write");
+					else {
+						perror("write error");
+						exit(-1);
+		      		}
+		    	}
+		    	if(*buf=='0'){
+				lectura = true;
+				break;
+				}
+			}
+		}
+
+		while(lectura){
+			cout << "Entre al modo lectura en client" << endl;
+			if ( (rc=read(fd,buf,sizeof(buf))) > 0) {
+				printf("read %u bytes: %.*s\n", rc, rc, buf);
+			}
+			if (rc == -1) {
+				perror("read");
+			exit(-1);
+			}
+			else if (rc == 0) {
+				printf("EOF\n");
+				close(fd);
+			}
+			if(*buf=='0'){
+				lectura = false;
+			}
+		}
 	}
 
 	return 0;	
