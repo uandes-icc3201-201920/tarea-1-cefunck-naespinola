6aae3e39ac80e8b29a51f2e1e7243acd54f76aae
Cristian Funck <cefunck@miuandes.cl>
2019-09-23 06:07:27 -0300

    Actualización de client y server

--- client.cpp
+++ client.cpp
@@ -2,6 +2,8 @@
 #include <memory>
 #include <string>
 #include <unistd.h>
+#include <algorithm>
+#include <math.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include "util.h"
@@ -81,12 +83,168 @@ int connection_attempt(char *socket_path){
 	return fd;
 }
 
+string to_lower(string str){
+	transform(str.begin(), str.end(),str.begin(), ::tolower);
+	return str;
+}
+
+bool is_valid_key(string str_key){
+	unsigned long key;
+	if(str_key.front() != '-'){
+		try{
+			key = stoi(str_key);
+			if( key > 0 && (floor(key) == key))
+				return true;
+			}catch(const std::invalid_argument& ia){
+				return false;
+			}
+			return false;
+		}
+	}
+
+bool validate_statement(vector<string> splitted_statement){
+	vector<string> commands_without_args{"connect","disconnect","quit","list"};
+	vector<string> commands_with_1_arg{"connect","insert","get","peek","delete"};
+	vector<string> commands_with_2_arg{"insert","update"};
+	string command;
+	string key;
+	if(splitted_statement.size() < 1){
+		return false;
+	}
+	command = to_lower(splitted_statement[0]);
+	if(splitted_statement.size() > 1){
+		key = splitted_statement[1];
+	}
+	switch (splitted_statement.size()) {
+		case 1:
+			if(find(commands_without_args.begin(), commands_without_args.end(),command)!=commands_without_args.end()){
+				return true;
+			}
+			return false;
+		case 2:
+			if(find(commands_with_1_arg.begin(), commands_with_1_arg.end(),command)!=commands_with_1_arg.end()){
+				if(command == "insert" || command == "connect" || is_valid_key(key)){
+					return true;
+				}
+			}
+			return false;
+		case 3:
+			if(find(commands_with_2_arg.begin(), commands_with_2_arg.end(),command)!=commands_with_2_arg.end()){
+				if(is_valid_key(key)){
+					return true;
+				}
+			}
+			return false;
+		default:
+			return false;
+			break;
+	}
+
+}
+
+vector<string> split_by_delimiter(string str, string delimiter){
+    vector<string>result;
+  	int pos = str.find(delimiter);
+		string first = str.substr(0,pos);
+		string second = str.substr(pos+1);
+		if(first.size() > 0){
+			result.push_back(first);
+		}
+		if(second != first && second.size() > 0){
+			result.push_back(second);
+		}
+		return result;
+}
+
+vector<string> split_statement(string statement){
+		vector<string> result;
+		vector<string> empty_result;
+		result = split_by_delimiter(statement,"(");
+		if(result.size() == 1){
+			return result;
+		}
+		vector<string> temp = split_by_delimiter(result[1],",");
+		result[1] = temp[0].substr(0,temp[0].size()-1);
+		if(temp.size() == 1){
+			return result;
+		}
+		result[1] = temp[0].substr(0,temp[0].size());
+		if(temp.size() == 2){
+			result.push_back(temp[1].substr(0,temp[1].size()-1));
+			return result;
+		}
+		return empty_result;
+}
+
+int new_socket_connect(string socket_path){
+	int fd;
+	if(socket_path == ""){
+		socket_path = "/tmp/db.tuples.sock";
+	}
+	int path_length = socket_path.length();
+	char temp_path[path_length +1 ];
+	strncpy(temp_path,socket_path.c_str(),path_length);
+	fd = connection_attempt(temp_path);
+	return fd;
+}
+
+int disconnect(int fd){
+	if(fd != -1){
+	close(fd);
+	}
+}
+void quit(int fd){
+	cout << "Cerrando proceso cliente" << endl;
+	if (fd!=NULL && fd!=-1){
+		disconnect(fd);
+	}
+	exit(-1);
+}
+
+string command_code(string command){
+	if(command == "connect"){
+		return "1";
+	}else if(command == "disconnect"){
+		return "2";
+	}else if(command == "quit"){
+		return "3";
+	}else if(command == "insert"){
+		return "4";
+	}else if(command == "get"){
+		return "5";
+	}else if(command == "peek"){
+		return "6";
+	}else if(command == "update"){
+		return "7";
+	}else if(command == "delete"){
+		return "8";
+	}else if(command == "list"){
+		return "9";
+	}
+}
+
+
+string exec_statement(string user_input){
+	vector<string> splitted_input = split_statement(user_input);
+	if(splitted_input.size() > 0 && splitted_input.size() < 4 && validate_statement(splitted_input)){
+		switch (splitted_input.size()) {
+			case 1:
+				return ("0 " + command_code(splitted_input[0]));
+			case 2:
+				return ("0 " + command_code(splitted_input[0]) + " " +  splitted_input[1]);
+			case 3:
+				return ("0 " + command_code(splitted_input[0]) + " " +  splitted_input[1] + " " +  splitted_input[2]);
+		}
+	}
+	return "Sentencia Invalida";
+}
 
 int main(int argc, char** argv) {
 	char *socket_path = "/tmp/db.tuples.sock";
 	struct sockaddr_un addr;
 	char buf[100];
 	int fd,rc, opt;
+	bool is_connected;
 
 	// Procesar opciones de linea de comando
     while ((opt = getopt (argc, argv, "s:")) != -1) {
@@ -101,34 +259,84 @@ int main(int argc, char** argv) {
           }
     }
 
+
 		fd = connection_attempt(socket_path);
-		bool read_mode = true;
+		if(fd != -1){
+			is_connected = true;
+		}
+		else{
+			is_connected = false;
+		}
+		bool read_mode = false;
 		while(1){
 
-		while(!read_mode){
+			while(!is_connected){
 
 			cout << "Modo escritura client" << endl;
 			while( (rc=read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
-				if (write(fd, buf, rc) != rc) {
-					if (rc > 0){
-						fprintf(stderr,"partial write");
-					}else {
-						perror("write error");
-						exit(-1);
-		      		}
-		    	}
-
-		    if(*buf=='0'){
-					read_mode = true;
-					break;
+				string temp1(buf);
+				string user_input1 = temp1.substr(0,rc-1);
+				string client_output1 = exec_statement(user_input1);
+				int pos = client_output1.find("1");
+				string new_path = client_output1.substr(pos);
+				std::cout << "path:"<< new_path << '\n';
+				if(client_output1 == "0 1"){
+					fd = new_socket_connect("");
+					std::cout << "fd:"<<fd << '\n';
+					is_connected = true;
+				}else if(client_output1 == "0 3"){
+					fd = new_socket_connect(new_path);
+					is_connected = true;
+				}
+				if(client_output1 == "Sentencia Invalida"){
+					std::cout << "Error: " << client_output1 << endl;
+				}else if(client_output1.substr(0,pos+1) != "0 1"){
+					std::cout << "Error: Sentencia Invalida" << endl;
 				}
 			}
 		}
 
-		while(read_mode){
+			while(!read_mode && is_connected){
+
+			cout << "Modo escritura client" << endl;
+			while( (rc=read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
+				string temp(buf);
+				string user_input = temp.substr(0,rc-1);
+				string client_output = exec_statement(user_input);
+				if(client_output == "0 2"){
+					disconnect(fd);
+				}else if(client_output == "0 3"){
+					quit(fd);
+				}
+				if(client_output == "Sentencia Invalida"){
+					std::cout << "Error: " << client_output << '\n';
+				}
+				else{
+					rc = client_output.length();
+					const void * temp_buf = client_output.c_str();
+					if (write(fd, temp_buf, rc) != rc) {
+						if (rc > 0){
+							fprintf(stderr,"partial write");
+						}else {
+							perror("write error");
+							exit(-1);
+			      		}
+			    	}
+
+			    if(client_output.front()=='0'){
+						read_mode = true;
+						break;
+					}
+				}
+			}
+		}
+
+			while(read_mode && is_connected){
 			cout << "Modo lectura client" << endl;
 			if ( (rc=read(fd,buf,sizeof(buf))) > 0) {
-				printf("read %u bytes: %.*s\n", rc, rc, buf);
+				string server_output(buf);
+				cout << server_output.substr(2,rc-2) << endl;
+				//printf("read %u bytes: %.*s\n", rc, rc, buf);
 			}
 			if (rc == -1) {
 				perror("read");
@@ -142,7 +350,7 @@ int main(int argc, char** argv) {
 				read_mode = false;
 			}
 		}
-	}
+		}
 
 	return 0;
 }
--- server.cpp
+++ server.cpp
@@ -6,7 +6,7 @@
 #include <ctime>
 #include <math.h>
 #include "util.h"
-
+#include <pthread.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -15,6 +15,17 @@
 
 
 using namespace std;
+struct ClientThreadData {
+	//pthread_rwlock_t lock;
+	bool read_mode;
+	int rc, cl;
+	char buf[100];
+};
+
+// Almacenamiento KV
+KVStore db;
+unsigned long global_key_counter;
+
 
 int server_socket_file_descriptor(int listen_len_queue, char* socket_path){
 	struct sockaddr_un addr;
@@ -46,16 +57,13 @@ int server_socket_file_descriptor(int listen_len_queue, char* socket_path){
 	return fd;
 }
 
-// Almacenamiento KV
-KVStore db;
-unsigned long global_key_counter;
-
 
 Value string_to_value(string str_data){
 	vector<byte> data(str_data.begin(),str_data.end());
 	Value value = { str_data.size(), data};
 	return value;
 }
+
 
 string value_to_string(Value value){
 	string str_data(value.data.begin(), value.data.end());
@@ -107,11 +115,11 @@ bool update_into_db(unsigned long key, Value value){
 Value get(unsigned long key){
 	if(peek(key))
 		return db[key];
-	throw invalid_argument( "the key entered does not exist in database" );
+	Value error = string_to_value("Error: no existe la clave ingresada");
+	return error;
 }
 
 
-// Función que retorna una clave aleatorea entera entre 10.000 y 1000.
 unsigned long random_key(){
 	srand((unsigned)time(0));
 	unsigned long i;
@@ -119,15 +127,13 @@ unsigned long random_key(){
 	return i;
 }
 
-// Función que inicializa la base de datos.
+
 void init_db(){
 	global_key_counter = random_key();
 }
 
-// Función que inserta un dato en la clave indicada, validando primero si ya
-// existiese la clave, en tal caso no la inserta y retorna -1, en caso contrario
-// la inserta exitosamente el dato en la clave indicada y retorna la clave.
-int insert_into_db(unsigned long key, Value value){
+
+int insert_key_value_into_db(unsigned long key, Value value){
 	if (peek(key) || !is_valid_key(key)){
 		return -1;
 	}
@@ -135,29 +141,183 @@ int insert_into_db(unsigned long key, Value value){
 	return key;
 }
 
-// Función que inserta un dato en la base de datos con clave autogenerada, si
-// la operación tiene exito retorna la clave generada.
-int insert_into_db(Value value){
+
+int insert_value_into_db(Value value){
 	unsigned long new_key = global_key_counter;
-	int result = insert_into_db(new_key, value);
+	int result = insert_key_value_into_db(new_key, value);
 	if (result == new_key){
 		global_key_counter++;
 	}
 	return result;
 }
 
+vector<string> split(string str, string token){
+    vector<string>result;
+    while(str.size()){
+        int index = str.find(token);
+        if(index!=string::npos){
+            result.push_back(str.substr(0,index));
+            str = str.substr(index+token.size());
+            if(str.size()==0)result.push_back(str);
+        }else{
+            result.push_back(str);
+            str = "";
+        }
+    }
+    return result;
+}
+
+string exec_statement(string statement){
+	string response = "0 ";
+	unsigned long key;
+	Value value;
+	int integer_result;
+	string str_result;
+	vector<string> splitted_statement = split(statement," ");
+	switch (stoi(splitted_statement[1])) {
+		case 1:// connect
+			return response + "cliente ya está conectado";
+			break;
+		case 2:// disconnect
+			return response + "el cliente debe cerrar su socket";
+			break;
+		case 3:// quit
+			return response + "el cliente debe cerrar su socket y matar el proceso";
+			break;
+		case 4:// insert
+			if(splitted_statement.size() == 3){
+				//std::cout << "value:" << splitted_statement[2] << '\n';
+				value = string_to_value(splitted_statement[2]);
+				integer_result = insert_value_into_db(value);
+				if(integer_result != -1){
+					response = response + to_string(integer_result);
+				}else{
+					response = response + "Error: clave invalida";
+				}
+			}else if(splitted_statement.size() == 4){
+				//std::cout << "key:"<<splitted_statement[2] << '\n';
+				//std::cout << "value:"<<splitted_statement[3] << '\n';
+				key = stoi(splitted_statement[2]);
+				value = string_to_value(splitted_statement[3]);
+				integer_result = insert_key_value_into_db(key,value);
+				if(integer_result != -1){
+					response = response + to_string(integer_result);
+				}else{
+					response = response + "Error: clave invalida";
+				}
+			}
+			return response;
+			break;
+		case 5:// get
+			key = stoi(splitted_statement[2]);
+			value = get(key);
+			response = response + value_to_string(value);
+			return response;
+			break;
+		case 6:// peek
+			key = stoi(splitted_statement[2]);
+			if(peek(key)){
+				response = response + "true";
+			}else{
+				response = response + "false";
+			}
+			return response;
+			break;
+		case 7:// update
+			key = stoi(splitted_statement[2]);
+			value = string_to_value(splitted_statement[3]);
+			if(update_into_db(key,value)){
+				response = response + "actualización exitosa";
+			}else{
+				response = response + "Error: clave invalida para actualización";
+			}
+			return response;
+			break;
+		case 8:// delete
+			key = stoi(splitted_statement[2]);
+			if(delete_from_db(key) == 0){
+				response = response + "Eliminación exitosa";
+			}
+			else{
+				response = response + to_string(key);
+			}
+			return response;
+			break;
+		case 9:// list
+			vector<unsigned long> keys = list();
+			for(vector<unsigned long>::iterator it = keys.begin(); it != keys.end(); ++it) {
+				str_result += to_string(*it) + " ";
+			}
+			return response + str_result;
+			break;
+	}
+}
+
+void * command_processing(void * client_thread_data){
+	struct ClientThreadData *ct_data = (struct ClientThreadData *)client_thread_data;
+	bool read_mode = ct_data->read_mode;
+	char buf[100];
+	strncpy(buf, ct_data->buf,sizeof(ct_data->buf));
+	int rc = ct_data->rc;
+	int cl = ct_data->cl;
+	string client_output = "";
+	string server_output = "";
+	while (1){
+		while(!read_mode){
+			cout << "Modo escritura server" << endl;
+			//std::cout << "para el cliente:" << cl << endl;
+			if(client_output != ""){
+				server_output = exec_statement(client_output);
+				rc = server_output.length();
+				const void * temp_buf = server_output.c_str();
+				if (write(cl, temp_buf, rc) != rc){
+					if (rc > 0){
+						fprintf(stderr,"partial write");
+					} else {
+						perror("write error");exit(-1);
+					}
+				}
+				if(*buf=='0'){
+					read_mode = true;
+					//client_output = "";
+					break;
+				}
+			}
+		}
+
+		while(read_mode){
+			cout << "Modo lectura server" << endl;
+			//std::cout << "para el cliente:" << cl << endl;
+			if ( (rc=read(cl,buf,sizeof(buf))) > 0) {
+				//printf("read %u bytes: %.*s\n", rc, rc, buf);
+				string temp(buf);
+				client_output = temp.substr(0,rc);
+				//std::cout << "client_output" << client_output << '\n';
+			}
+			if (rc == -1) {
+				perror("read");
+				exit(-1);
+			}
+			else if (rc == 0) {
+				printf("EOF\n");
+				read_mode = false;
+				//close(cl);
+			}
+			if(*buf=='0'){
+				read_mode = false;
+			}
+		}
+	}
+}
 
 int main(int argc, char** argv) {
-
-	char buf[100];
-  	int fd,cl,rc;
-
+	init_db();
+	vector<pthread_t> thread_ids;
 	char *socket_path = "/tmp/db.tuples.sock";
-
-	int opt;
+  int fd,rc,opt;
 
 	// Procesar opciones de linea de comando
-    while ((opt = getopt (argc, argv, "s:")) != -1) {
+	while ((opt = getopt (argc, argv, "s:")) != -1) {
         switch (opt)
 		{
 			/* Procesar el flag s si el usuario lo ingresa */
@@ -168,84 +328,25 @@ int main(int argc, char** argv) {
 				return EXIT_FAILURE;
           }
     }
-    fd = server_socket_file_descriptor(5, socket_path);
-
-    
-	if ( (cl = accept(fd, NULL, NULL)) == -1) {
-	perror("accept error");
-	}
-
-
-	//Intento de read
-
-	bool lectura = false;
-
-	while (1) {
-
-		while(!lectura){
-			cout << "Modo escritura server" << endl;
-			while( (rc=read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
-				if (write(cl, buf, rc) != rc) {
-					if (rc > 0) fprintf(stderr,"partial write");
-					else {
-						perror("write error");
-						exit(-1);
-		      		}
-		    	}
-		    	if(*buf=='0'){
-					lectura = true;
-					break;
-				}
-		  	}	
+  fd = server_socket_file_descriptor(99, socket_path);
+	while(1){
+		char buf[100];
+		int cl = accept(fd, NULL, NULL);
+		if(cl == -1){
+			perror("accept error");
+		}else{
+			bool read_mode = true;
+			struct ClientThreadData *ct_data;
+			ct_data = (struct ClientThreadData *)malloc(sizeof(struct ClientThreadData));
+			ct_data->read_mode = read_mode;
+			//ct_data->buf = buf;
+			strncpy(ct_data->buf, buf, sizeof(buf));
+			ct_data->rc = rc;
+			ct_data->cl = cl;
+			pthread_t new_client_thread;
+			pthread_create(&new_client_thread,NULL,command_processing,(void *)ct_data);
+			thread_ids.push_back(new_client_thread);
 		}
-
-		while(lectura){
-			cout << "Modo lectura server" << endl;
-			if ( (rc=read(cl,buf,sizeof(buf))) > 0) {
-				printf("read %u bytes: %.*s\n", rc, rc, buf);
-			}
-			if (rc == -1) {
-				perror("read");
-				exit(-1);
-			}
-			else if (rc == 0) {
-				printf("EOF\n");
-				close(cl);
-			}
-			if(*buf=='0'){
-				lectura = false;
-			}
-		}
-	}
-
-	init_db();
-
-	cout << socket_path << endl;
-
-	Value value1 = string_to_value("primer dato");
-	Value value2 = string_to_value("segundo dato");
-	Value value3 = string_to_value("tercer dato");
-	insert_into_db(1000,value1);
-	insert_into_db(1001,value2);
-	insert_into_db(1001,value2);
-	insert_into_db(value2);
-	insert_into_db(value3);
-	update_into_db(1001,string_to_value("dato actualizado"));
-
-	cout << db.size() << endl;
-	if (delete_from_db(1000) == 0)
-		cout << "deleted" << endl;
-	else
-		cout << "cant delete" << endl;
-	cout << db.size() << endl;
-
-
-	vector<unsigned long> keys = list();
-	for(vector<unsigned long>::iterator it = keys.begin(); it != keys.end(); ++it) {
-		unsigned long key = *it;
-		Value value = get(key);
-		string str_value = value_to_string(value);
-		cout << "clave:" << *it << " valor:" << str_value << endl;
 	}
 
 	return 0;
